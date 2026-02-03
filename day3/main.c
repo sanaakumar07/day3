3f3406eec99ef43c861907e78586e5a408b82fe1
sanaa <Sanaakumar07@gmail.com>
2026-02-03 16:31:52 +0530

    c

--- day3/main.c
+++ day3/main.c
@@ -1,15 +1,39 @@
-//
-//  main.c
-//  day3
-//
-//  Created by Sanaa Kumar on 03/02/26.
-//
-
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+int main(void)
+{
+    int n, i, k;
+    int a[100];
+    int c = 0;
+    int flag = 0;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &k);
+
+    for (i = 0; i < n; i++)
+    {
+        c++;
+        if (a[i] == k)
+        {
+            printf("Found at index %d\n", i);
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+    {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d\n", c);
+
+    return 0;
 }
+
